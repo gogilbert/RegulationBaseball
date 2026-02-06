0d3a6b150cfc42431b5f75c5ecd2bbace4300528
gogilbert <jsboersema@hotmail.com>
2026-02-06 10:22:38 -0500

    Implement Click and Drag

--- Scripts/Pitcher.cs
+++ Scripts/Pitcher.cs
@@ -10,22 +10,28 @@ public partial class Pitcher : Node3D
 	public PackedScene BaseballScene { get; set; }
 
 	private bool isAiming = false;
+	private Vector2 mousevelocity;
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
         if (Input.IsActionJustPressed("Pitch")) {
 			isAiming = true;
 		}
 
 		if (Input.IsActionJustReleased("Pitch")) {
-			Vector2 mouseVelocity = Input.GetLastMouseVelocity();
-			GD.Print(mouseVelocity);
+			GD.Print(mousevelocity);
 			ThrowBall();
 			isAiming = false;
 		}
     }
 
-	public void ThrowBall(){
+    public override void _Input(InputEvent @event)
+    {
+        if (isAiming && @event is InputEventMouseMotion mouseMotion){
+			mousevelocity = mouseMotion.Relative;
+		}
+    }
+    public void ThrowBall(){
 		Node3D newBaseball = (Node3D) BaseballScene.Instantiate();
 
 		newBaseball.Position = BaseballSpawnPoint.Position;
