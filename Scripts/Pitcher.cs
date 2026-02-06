cd02524dc28a963010470ef6a05289a19dad4bf7
gogilbert <jsboersema@hotmail.com>
2026-02-06 10:35:44 -0500

    Put pitching on physics tick

--- Scripts/Pitcher.cs
+++ Scripts/Pitcher.cs
@@ -10,23 +10,31 @@ public partial class Pitcher : Node3D
 	public PackedScene BaseballScene { get; set; }
 
 	private bool isAiming = false;
+	private bool throwOnTick = false;
 	private Vector2 mousevelocity;
 
     public override void _PhysicsProcess(double delta)
     {
-        if (Input.IsActionJustPressed("Pitch")) {
-			isAiming = true;
-		}
-
-		if (Input.IsActionJustReleased("Pitch")) {
+		// Ensures physics can sync across clients
+        if (throwOnTick) {
 			GD.Print(mousevelocity);
-			ThrowBall();
 			isAiming = false;
+			throwOnTick = false;
+			ThrowBall();
 		}
     }
 
+	// Mouse speed and pitch release queued up later on physics tick
     public override void _Input(InputEvent @event)
     {
+        if (@event.IsActionPressed("Pitch")) {
+			isAiming = true;
+		}
+
+		if (@event.IsActionReleased("Pitch")) {
+			throwOnTick = true;
+		}
+
         if (isAiming && @event is InputEventMouseMotion mouseMotion){
 			mousevelocity = mouseMotion.Relative;
 		}
